ab79e26622e66225097fdbca8af4d390e34a4a20
Gabriel Martins <gabriel.hamartins@gmail.com>
2021-08-16 17:23:07 -0300

    fixed first test

--- src/TesteUm/Main.java
+++ src/TesteUm/Main.java
@@ -40,14 +40,14 @@ public class Main {
             }
         }
 
-        System.out.println("A quantidade de pessoas até 14 anos é " + faixaUm);
-        System.out.println("A quantidade de pessoas de 15 a 29 anos é " + faixaDois);
-        System.out.println("A quantidade de pessoas de 30 a 44 anos é " + faixaTres);
+        System.out.println("A quantidade de pessoas com até 14 anos é " + faixaUm);
+        System.out.println("A quantidade de pessoas entre 15 a 29 anos é " + faixaDois);
+        System.out.println("A quantidade de pessoas entre 30 a 44 anos é " + faixaTres);
         System.out.println("A quantidade de pessoas entre de 45 a 59 anos " + faixaQuatro);
-        System.out.println("A quantidade de pessoas entre acima de 60 anos é " + faixaCinco);
+        System.out.println("A quantidade de pessoas acima de 60 anos é " + faixaCinco);
 
-        System.out.println("A porcentagem de pessoas ate 14 anos é " + ((double)faixaUm/10));
-        System.out.println("A porcentagem de pessoas acima de 60 anos é " + ((double)faixaCinco/10));
+        System.out.println("A porcentagem de pessoas com até 14 anos é de " + ((double)faixaUm/10) * 100 + "%");
+        System.out.println("A porcentagem de pessoas acima de 60 anos é de " + ((double)faixaCinco/10) * 100 + "%");
 
 
     }
